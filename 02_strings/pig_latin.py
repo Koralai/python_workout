99c1f59351c967e34e5d4d7f3a3d61cae76fe410
Carrie Clark <carolyn.anne.clark@gmail.com>
2023-06-22 16:13:36 -0500

    pig latin, maintain capitalization

--- 02_strings/pig_latin.py
+++ 02_strings/pig_latin.py
@@ -1,13 +1,24 @@
 def pig_latin(word):
-    """Take a word and convert it to pig latin"""
+    """Take a word and convert it to pig latin, maintaining capitalization"""
     
+    # check if the first letter is capitalized
+    first_letter_capped = False
+    if word[0].isupper() == True:
+        first_letter_capped = True
+            
     # if the word starts in a vowel, add "way" to the end
     if word[0].lower() in 'aeiouy':
-        return f"{word}way"
+        word = f"{word}way"
     
     # otherwise, put the first letter at the end of the word and add "ay"
-    return f"{word[1:]}{word[0]}ay"
+    else:
+        word = f"{word[1:]}{word[0]}ay"
     
-print(pig_latin("happy"))
+    if first_letter_capped == True:
+        return word.capitalize()
+    return word
+    
+print(pig_latin("Happy"))
 print(pig_latin("awesome"))
 print(pig_latin("youthful"))
+print(pig_latin("Mary"))

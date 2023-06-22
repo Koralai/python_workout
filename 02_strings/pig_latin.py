41e1989af78805445a401f5e0bb8a2d3ef7998c1
Carrie Clark <carolyn.anne.clark@gmail.com>
2023-06-22 16:06:04 -0500

    pig latin - make more concise

--- 02_strings/pig_latin.py
+++ 02_strings/pig_latin.py
@@ -3,14 +3,11 @@ def pig_latin(word):
     
     # if the word starts in a vowel, add "way" to the end
     if word[0].lower() in 'aeiouy':
-        word += "way"
-        return word
+        return f"{word}way"
     
-    # otherwise, put the first letter at the end of the work and add "ay"
-    else:
-        first_letter = word[0]
-        word = word[1:] + first_letter + "ay"
-        return word
+    # otherwise, put the first letter at the end of the word and add "ay"
+    return f"{word[1:]}{word[0]}ay"
     
 print(pig_latin("happy"))
 print(pig_latin("awesome"))
+print(pig_latin("youthful"))

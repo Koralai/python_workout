dab85618e301220db06c6625f8a063f405b9eae5
Carrie Clark <carolyn.anne.clark@gmail.com>
2023-06-22 16:33:03 -0500

    incorporate pig latin logic into pl_sentence func

--- 02_strings/pig_latin_sentence.py
+++ 02_strings/pig_latin_sentence.py
@@ -1,20 +1,13 @@
-def pig_latin(word):
-    if word[0].lower() in 'aeiouy':
-        return f"{word}way"
-    
-    return f"{word[1:]}{word[0]}ay"
-
 def pl_sentence(sentence):
-    """
-    Take a series of words, separated by spaces, and convert them
-    into pig latin
-    """    
-    sentence_words = sentence.split()
+    """Take a series of words and convert them into pig latin"""
+
     pl_sentence_words = []
     
-    for word in sentence_words:
-        word = pig_latin(word)
-        pl_sentence_words.append(word)
+    for word in sentence.split():
+        if word[0].lower() in 'aeiouy':
+            pl_sentence_words.append(f"{word}way")
+        else:
+            pl_sentence_words.append(f"{word[1:]}{word[0]}ay")
         
     return ' '.join(pl_sentence_words)
 

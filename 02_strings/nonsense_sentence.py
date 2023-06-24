bc7dde47c35b42e4d64598c102bafba6d835ea0e
Carrie Clark <carolyn.anne.clark@gmail.com>
2023-06-24 13:17:39 -0500

    nonsense_sentence - start at random word

--- 02_strings/nonsense_sentence.py
+++ 02_strings/nonsense_sentence.py
@@ -1,19 +1,24 @@
 """Read a text file and return a nonsensical sentence from words in the file"""
 
-def nonsense_sentence(file, start_with_nth_word, words_in_sentence, nth_step):
+import random
+
+def nonsense_sentence(file, words_in_sentence, every_nth_word):
     
     file_text = open(file, encoding='utf-8').read()    
     file_text_words = file_text.split()    
     
     nonsense_sentence_words = []
-    first_word = start_with_nth_word
-    total_steps = nth_step * words_in_sentence
     
-    for i in range(first_word, total_steps, nth_step):
+    starting_word_index_range = round(len(file_text_words) * 0.6)
+    starting_word_index = random.randint(0, starting_word_index_range)
+    
+    last_word_index = starting_word_index + (words_in_sentence * every_nth_word)
+    
+    for i in range(starting_word_index, last_word_index, every_nth_word):
         nonsense_sentence_words.append(file_text_words[i])
     
     final_sentence = ' '.join(nonsense_sentence_words)
     
     return f"{final_sentence.capitalize()}."
 
-print(nonsense_sentence('sun_also_rises.txt', 68, 15, 20))
+print(nonsense_sentence('sun_also_rises.txt', 15, 20))

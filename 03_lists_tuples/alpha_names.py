9b84e0f43a3efb8495fd8b369fa15c48069c091c
Carrie Clark <carolyn.anne.clark@gmail.com>
2023-06-28 14:34:52 -0500

    alpha_names: works for last names only

--- 03_lists_tuples/alpha_names.py
+++ 03_lists_tuples/alpha_names.py
@@ -1,38 +1,22 @@
-def compare_words(word_1, word_2):
-    """Compare two words and return them in a list, in alpha order"""
-    
-    output = []
-    
-    i = 0
-    while True:
-        word_1_lett = word_1.lower()[i]
-        word_2_lett = word_2.lower()[i]
-        
-        if ord(word_1_lett) < ord(word_2_lett):
-            output = [word_1, word_2]
-            break
-        if ord(word_2_lett) < ord(word_1_lett):
-            output = [word_2, word_1]
-            break
-        i += 1
-    
-    return output
-    
-
-def alphabetize_names(list_of_dicts):
+def alphabetize_names(list_of_people):
     """
     Take a list of dictionaries containing first and last names.
     Return the same list of dictionaries sorted alphabetically 
     (by last name, then by first name).
     """
     
-    output = []
+    return sorted(list_of_people, key=lambda person: person['last'])
     
-    
-
+PEOPLE = [
+    {'first': 'frodo', 'last': 'baggins', 'age': 50,},
+    {'first': 'bilbo', 'last': 'baggins', 'age': 111,},
+    {'first': 'samwise', 'last': 'gamgee', 'age': 38, },
+    {'first': 'meriadoc', 'last': 'brandybuck', 'age': 36, },
+    {'first': 'peregrin', 'last': 'took', 'age': 29,},
+]    
 
 def main():
-    print(compare_words('acrimonious', 'acre'))
+    print(alphabetize_names(PEOPLE))
 
 if __name__ == '__main__':
     main()

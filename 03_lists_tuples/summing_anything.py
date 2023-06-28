5ffd58bdb0fe1aa9a7b67fcc0047c1c9cd91920b
Carrie Clark <carolyn.anne.clark@gmail.com>
2023-06-28 13:08:03 -0500

    summing_anything, return () if no args are given

--- 03_lists_tuples/summing_anything.py
+++ 03_lists_tuples/summing_anything.py
@@ -1,17 +1,21 @@
 def get_any_sum(*args):
     """Return the sum of the arguments, of any quantity and type"""
     
-    output = args[0] # Get an initial value that is type-agnostic
+    if not args:        
+        return args
     
+    output = args[0]    # Set an initial value that is type-agnostic    
     for arg in args[1:]:
-        output += arg
-    
+        output += arg    
     return output
+
 
 def main():
     print(get_any_sum('abc','def'))
     print(get_any_sum(1,3,5))
     print(get_any_sum([123],[456],[789]))
+    print(get_any_sum())
+    print(get_any_sum('just one string'))
 
 if __name__ == '__main__':
     main()

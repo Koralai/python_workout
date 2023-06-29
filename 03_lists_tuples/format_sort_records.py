5eb26e25e2fc0ea5d14b431b8fa45ba0fd38827e
Carrie Clark <carolyn.anne.clark@gmail.com>
2023-06-29 15:04:49 -0500

    format_sort_records - add pipes

--- 03_lists_tuples/format_sort_records.py
+++ 03_lists_tuples/format_sort_records.py
@@ -14,9 +14,9 @@ def get_formatted_records(records):
         last_name_col_space = 10 - (len(last_name))
         time_col_space = 5 - (len(time_2_dec))
         
-        formatted_rec = (f"{last_name}{' ' * last_name_col_space}"
-                         f"{first_name}{' ' * first_name_col_space}"
-                         f"{' ' * time_col_space}{time_2_dec}")
+        formatted_rec = (f"| {last_name}{' ' * last_name_col_space}"
+                         f"| {first_name}{' ' * first_name_col_space}"
+                         f"| {' ' * time_col_space}{time_2_dec}")
         
         if rec == records[-1]: # no line break after last record
             output += formatted_rec

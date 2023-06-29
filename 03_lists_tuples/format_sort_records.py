24e6a101f76f199a0679a35c4e68ca9f45d4e6d1
Carrie Clark <carolyn.anne.clark@gmail.com>
2023-06-29 15:00:57 -0500

    formatting works, not the clearest to read yet

--- 03_lists_tuples/format_sort_records.py
+++ 03_lists_tuples/format_sort_records.py
@@ -6,7 +6,14 @@ def get_formatted_records(records):
     output = ''
     
     for rec in records:
-        formatted_rec = f"{rec[1]} {rec[0]} {rec[2]:.2f}"
+        first_name_col_space = 10 - (len(rec[0]))
+        last_name_col_space = 10 - (len(rec[1]))
+        time_col_space = 5 - (len(f"{rec[2]:.2f}"))
+        
+        formatted_rec = (f"{rec[1]}{' ' * last_name_col_space}"
+                         f"{rec[0]}{' ' * first_name_col_space}"
+                         f"{' ' * time_col_space}{rec[2]:.2f}")
+        
         if rec == records[-1]: # no line break after last record
             output += formatted_rec
         else:   

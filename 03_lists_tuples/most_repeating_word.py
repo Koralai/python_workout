f4b1c01aacfcc3b1a1279ed40667167086e710ba
Carrie Clark <carolyn.anne.clark@gmail.com>
2023-06-29 14:18:24 -0500

    Move Counter logic to separate functions

--- 03_lists_tuples/most_repeating_word.py
+++ 03_lists_tuples/most_repeating_word.py
@@ -1,5 +1,16 @@
 from collections import Counter 
 
+def count_most_repeated_letter(word):
+    """
+    Find the most common letter in a word and return the number of times
+    it occurs
+    """
+    return Counter(word.lower()).most_common(1)[0][1]
+
+def get_most_repeated_letter(word):
+    """Return the most common letter in a word"""
+    return Counter(word.lower()).most_common(1)[0][0]
+
 def get_most_repeating_word(list_words):
     """
     Take a sequence of strings and return the string that contains the
@@ -11,16 +22,12 @@ def get_most_repeating_word(list_words):
     highest_letter_ct = 0
     
     for word in list_words:
-        # letters by how often they appear, in descending order:
-        letters_by_frequency = Counter(word.lower()).most_common()
-        
-        # number of times the most common letter appears in the word:
-        current_letter_ct = letters_by_frequency[0][1] 
+        current_letter_ct = count_most_repeated_letter(word)
         
         if current_letter_ct > highest_letter_ct:
             most_repeating_word = word
             highest_letter_ct = current_letter_ct
-            repeated_letter = letters_by_frequency[0][0]
+            repeated_letter = get_most_repeated_letter(word)
     
     message = (f'The word "{most_repeating_word}" contains the letter '
                 f'"{repeated_letter}" {highest_letter_ct} times.')

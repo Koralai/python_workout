1f6fe72db0299ca09ddc614e51446fc9b7f26826
Carrie Clark <carolyn.anne.clark@gmail.com>
2023-06-29 15:02:50 -0500

    format_sort_records - more readable

--- 03_lists_tuples/format_sort_records.py
+++ 03_lists_tuples/format_sort_records.py
@@ -6,13 +6,17 @@ def get_formatted_records(records):
     output = ''
     
     for rec in records:
-        first_name_col_space = 10 - (len(rec[0]))
-        last_name_col_space = 10 - (len(rec[1]))
-        time_col_space = 5 - (len(f"{rec[2]:.2f}"))
+        first_name = rec[0]
+        last_name = rec[1]
+        time_2_dec = f"{rec[2]:.2f}"
         
-        formatted_rec = (f"{rec[1]}{' ' * last_name_col_space}"
-                         f"{rec[0]}{' ' * first_name_col_space}"
-                         f"{' ' * time_col_space}{rec[2]:.2f}")
+        first_name_col_space = 10 - (len(first_name))
+        last_name_col_space = 10 - (len(last_name))
+        time_col_space = 5 - (len(time_2_dec))
+        
+        formatted_rec = (f"{last_name}{' ' * last_name_col_space}"
+                         f"{first_name}{' ' * first_name_col_space}"
+                         f"{' ' * time_col_space}{time_2_dec}")
         
         if rec == records[-1]: # no line break after last record
             output += formatted_rec

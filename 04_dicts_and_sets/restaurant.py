ba42befd2b8d15a1cd3587e767a34cd859c2608e
Carrie Clark <carolyn.anne.clark@gmail.com>
2023-06-30 13:16:50 -0500

    restaurant exercise

---
+++ 04_dicts_and_sets/restaurant.py
@@ -0,0 +1,42 @@
+MENU = {
+    'bubble tea': 4,
+    'herbal tea': 2,
+    'coffee': 3,
+    'espresso': 4,
+    'latte': 4,
+    'muffin': 4,
+    'scone': 3,
+    'breakfast burrito': 8,
+    'fruit cup': 4,
+    'yogurt parfait': 3
+}
+
+def take_order(menu):
+    """
+    Take user input for items ordered from the menu. Keep a running
+    total of the cost of the user's order.
+    """
+    print("Welcome to our cafe! Press 'enter' at any time to quit.\n")
+    
+    price = 0
+    items_ordered = []
+    
+    while True:
+        order = input("What would you like to order? ")
+        if not order:
+            break
+        if order not in menu:
+            print("Sorry, that's not available.\n")
+        else:
+            price += menu[order]
+            items_ordered.append(order)
+    
+    price_message = f"You have ordered {len(items_ordered)} items, for a total of ${price:.2f}."
+    
+    if price > 0:
+        return price_message
+    return None
+        
+
+print(take_order(MENU))
+     

3066464647bbeb14a9b6766afd3aff151073ab10
Carrie Clark <carolyn.anne.clark@gmail.com>
2023-07-12 16:11:40 -0500

    Refactor passwd_to_dict - splitlines() not needed

--- 05_files/passwd_to_dict.py
+++ 05_files/passwd_to_dict.py
@@ -2,20 +2,16 @@ def passwd_to_dict(filename):
     """
     Read a password file and return a dictionary of usernames and id numbers
     """
-    output = {}
+    users = {}
     
-    with open(filename, encoding='utf-8') as f:
-        lines = f.read().splitlines()
-    
-    for line in lines:
-        if line.startswith('#'):
-            continue
+    with open(filename, encoding='utf-8') as passwd:
+        for line in passwd:
+            if not line.startswith(('#', '\n')):
+                line_elements = line.split(':')
+                username = line_elements[0]
+                userid = int(line_elements[2])
+                users[username] = userid
         
-        line_elements = line.split(':')
-        username = line_elements[0]
-        userid = line_elements[2]
-        output[username] = userid
-        
-    return output
+    return users
 
 print(passwd_to_dict('05_files/passwd.txt'))

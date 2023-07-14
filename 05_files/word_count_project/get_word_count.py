0db8538f1e7f3dcc8338d53187db0655862c9ea7
Carrie Clark <carolyn.anne.clark@gmail.com>
2023-07-14 14:42:13 -0500

    move files to subfolder, tweaks to get_word_count

--- 05_files/word_count_project/get_word_count.py
+++ 05_files/word_count_project/get_word_count.py
@@ -1,10 +1,12 @@
 from get_words_set import get_words_set
 
-excluded_words = get_words_set('05_files/excluded_words.txt')
+excluded_words = get_words_set('05_files/word_count_project/excluded_words.txt')
 
-def get_most_frequent_words(filename):
+def get_word_count(filename):
     """
-    Read a file and return the most frequent, meaningful words in that file
+    Read a file and return a dictionary where the key is a word and the
+    value is the number of times that word appears in the file.
+    Allow for excluded words ("the", "a", etc).
     """
     word_count = {}
     
@@ -14,16 +16,17 @@ def get_most_frequent_words(filename):
             
             for word in words:
                 if word not in excluded_words:
-                    if word != 'I':
-                        word = word.lower()
+
                     # strip out initial and final punctuation
                     if word[-1] in '?!;.,:"”)':
                         word = word[:-1]
                     if word[0] in '("“':
                         word = word[1:]
+                        
+                    if word != 'I':
+                        word = word.lower()
+                        
                     # increment the count for that word 
                     word_count[word] = word_count.get(word, 0) + 1
     
     return word_count
-
-print(get_most_frequent_words('02_strings/sun_also_rises.txt'))

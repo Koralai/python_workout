958fa6926e3c4ec39d8c0410cd78a67efd068332
Carrie Clark <carolyn.anne.clark@gmail.com>
2023-07-16 10:50:12 -0500

    format_word_count_data function

--- 05_files/word_count_project/format_word_count_data.py
+++ 05_files/word_count_project/format_word_count_data.py
@@ -1,10 +1,22 @@
-def format_word_count_data(list_name):
+def format_word_count_data(list_name, num_rows=10):
     """
     Accepts word count data (in the form of a list of tuples: each tuple
-    contains the word and its count). Print the data in a table.
+    contains the word and its count). Print the data in a table, with the
+    specified number of rows (or 10 rows by default).
     """
     
-    output = ""
+    output = ("--------------------------------------\n"
+              "| WORD                      | # USES |\n"
+              "--------------------------------------\n")
     
+    i = 1
     for datum in list_name:
-        pass
+        if i <= num_rows:
+            word = datum[0]
+            count = datum[1]
+            output += f"| {word:25} | {count:6} |\n"
+            i += 1
+    
+    output += "--------------------------------------"
+    
+    return output

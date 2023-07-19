25c3cd7f267c3f15e6d1063dcc91842e009fd73f
Carrie Clark <carolyn.anne.clark@gmail.com>
2023-07-19 16:12:03 -0500

    find_longest_word function

---
+++ 05_files/find_longest_word.py
@@ -0,0 +1,17 @@
+def find_longest_word(filename):
+    """Read a file and return the longest word in that file"""
+    
+    with open(filename, encoding='utf-8') as new_file:
+        longest_word_length = 0
+        longest_word = ''
+        
+        for line in new_file:
+            words = line.split()
+            for word in words:
+                if len(word) > longest_word_length:
+                    longest_word = word
+                    longest_word_length = len(word)
+                    
+    return longest_word
+
+print(find_longest_word('02_strings/lamplighter.txt'))

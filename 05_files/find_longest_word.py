6038a70d68d223f16cade1fe4c86efef5909ab77
Carrie Clark <carolyn.anne.clark@gmail.com>
2023-07-19 16:37:11 -0500

    longest_word_length variable not needed

--- 05_files/find_longest_word.py
+++ 05_files/find_longest_word.py
@@ -13,16 +13,14 @@ def find_longest_word(filename):
     """Read a file and return the longest word in that file"""
     
     with open(filename, encoding='utf-8') as new_file:
-        longest_word_length = 0
         longest_word = ''
         
         for line in new_file:
             words = line.split()
             
             for word in words:
-                if len(word) > longest_word_length:
+                if len(word) > len(longest_word):
                     if exclude_word(word) is False:
                         longest_word = word
-                        longest_word_length = len(word)
                     
     return longest_word

37ab78a4e64f0368c297aa16459e15862d723b7b
Carrie Clark <carolyn.anne.clark@gmail.com>
2023-07-19 15:59:07 -0500

    move strip punctuation logic to own function

--- 05_files/word_count_project/get_word_count.py
+++ 05_files/word_count_project/get_word_count.py
@@ -2,6 +2,15 @@ from get_words_set import get_words_set
 
 excluded_words = get_words_set('05_files/word_count_project/excluded_words.txt')
 
+def strip_punctuation(word):
+    """Strip out initial and final punctuation from a word"""
+    if word[-1] in '?!;.,:"”)':
+        word = word[:-1]
+    if word[0] in '("“':
+        word = word[1:]
+    
+    return word
+
 def get_word_count(filename):
     """
     Read a file and return a dictionary where the key is a word and the
@@ -15,11 +24,7 @@ def get_word_count(filename):
             words = line.split()
             
             for word in words:
-                # strip out initial and final punctuation
-                if word[-1] in '?!;.,:"”)':
-                    word = word[:-1]
-                if word[0] in '("“':
-                    word = word[1:]
+                word = strip_punctuation(word)
                 
                 if word.lower() not in excluded_words:        
                     # increment the count for that word 

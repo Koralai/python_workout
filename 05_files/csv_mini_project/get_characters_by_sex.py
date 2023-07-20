2d5be36e64a656beceb21ffdc8cf53abc9d11d17
Carrie Clark <carolyn.anne.clark@gmail.com>
2023-07-20 10:13:45 -0500

    spacing/wording

--- 05_files/csv_mini_project/get_characters_by_sex.py
+++ 05_files/csv_mini_project/get_characters_by_sex.py
@@ -10,16 +10,18 @@ def get_characters_by_sex(filename_r, filename_w, sex='f'):
     
     with open(filename_r, encoding='utf-8') as character_file:
         csv_reader = csv.reader(character_file, delimiter=',')
+        
         for row in csv_reader:
             character_sex = row[3]
             if character_sex == sex:
                 first_name = row[0]
                 last_name = row[1]
-                race = row[2]
-                output_characters.append((first_name, last_name, race))
+                fantasy_race = row[2]
+                output_characters.append((first_name, last_name, fantasy_race))
                 
     with open(filename_w, 'w', newline='', encoding='utf-8') as new_character_file:
         csv_writer = csv.writer(new_character_file, delimiter='\t')
+        
         for character in output_characters:
             if character[1] == '': # no last name
                 csv_writer.writerow([character[0], character[2]])
@@ -30,5 +32,5 @@ def get_characters_by_sex(filename_r, filename_w, sex='f'):
                 
 get_characters_by_sex(
     '05_files/csv_mini_project/lotr_characters.csv',
-    '05_files/csv_mini_project/lotr_m_characters.csv', 'm'
+    '05_files/csv_mini_project/lotr_f_characters.csv', 'f'
     )

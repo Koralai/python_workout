1eb9334d9ace23f36a08dfe10b3a7506e464c4aa
Carrie Clark <carolyn.anne.clark@gmail.com>
2023-07-20 09:36:59 -0500

    passwd_to_csv - write file logic

--- 05_files/passwd_to_csv.py
+++ 05_files/passwd_to_csv.py
@@ -1,21 +1,24 @@
 import csv
 
-def passwd_to_csv(filename_r):
+def passwd_to_csv(filename_r, filename_w):
     """
-    Read csv data from the first file and write it to the second
-    file in formatted columns
+    Extract user data from a csv file and write it to another csv file
     """
     
     users = {}
     
-    with open(filename_r, encoding='utf-8') as csv_file:
-        csv_reader = csv.reader(csv_file, delimiter=':')
+    with open(filename_r, encoding='utf-8') as csv_file_r:
+        csv_reader = csv.reader(csv_file_r, delimiter=':')
         for row in csv_reader:
             if len(row) > 1: # exclude comments
                 username = row[0]
                 userid = row[2]
                 users[username] = userid
     
-    return users
+    with open(filename_w, 'w', newline='', encoding='utf-8') as csv_file_w:
+        csv_writer = csv.writer(csv_file_w, delimiter='\t')
+        for key, value in users.items():
+            csv_writer.writerow([key, value])
+    
             
-print(passwd_to_csv('05_files/passwd.csv'))
+passwd_to_csv('05_files/passwd.csv', '05_files/users.csv')

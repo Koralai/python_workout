0e55bb32dbe03a57435f1af5625b72419794dc31
Carrie Clark <carolyn.anne.clark@gmail.com>
2023-07-20 10:10:57 -0500

    logic works, though output messy

--- 05_files/csv_mini_project/get_characters_by_sex.py
+++ 05_files/csv_mini_project/get_characters_by_sex.py
@@ -1,16 +1,34 @@
 import csv
 
-def get_characters_by_sex(filename_r, sex='f'):
+def get_characters_by_sex(filename_r, filename_w, sex='f'):
     """
     Read a CSV file of character data. Write a CSV file containing only 
     the characters of the sex specified in the function call ('m' or 'f')
     """
+    
+    output_characters = []
     
     with open(filename_r, encoding='utf-8') as character_file:
         csv_reader = csv.reader(character_file, delimiter=',')
         for row in csv_reader:
             character_sex = row[3]
             if character_sex == sex:
-                print(row)
+                first_name = row[0]
+                last_name = row[1]
+                race = row[2]
+                output_characters.append((first_name, last_name, race))
                 
-get_characters_by_sex('05_files/csv_mini_project/lotr_characters.csv')
+    with open(filename_w, 'w', newline='', encoding='utf-8') as new_character_file:
+        csv_writer = csv.writer(new_character_file, delimiter='\t')
+        for character in output_characters:
+            if character[1] == '': # no last name
+                csv_writer.writerow([character[0], character[2]])
+            else:
+                csv_writer.writerow(character)
+                
+                
+                
+get_characters_by_sex(
+    '05_files/csv_mini_project/lotr_characters.csv',
+    '05_files/csv_mini_project/lotr_m_characters.csv', 'm'
+    )

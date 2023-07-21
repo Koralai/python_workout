49b13faa982996074d2e8c9a426597802360bfb8
Carrie Clark <carolyn.anne.clark@gmail.com>
2023-07-21 12:24:02 -0500

    logic to sort student scores by subject

--- 05_files/json/get_scores.py
+++ 05_files/json/get_scores.py
@@ -1,6 +1,22 @@
 import os
 import json
 
+def sort_scores_by_subject(list_of_students):
+    """
+    Take a list of student scores and return the scores sorted by subject
+    """
+    scores = {}
+    
+    for student in list_of_students:
+        for subject, score in student.items():
+            if subject not in scores:
+                scores[subject] = [score]
+            else:
+                scores[subject].append(score)
+            
+    return scores
+        
+
 def get_scores(dir_name):
     """
     Take the name of a directory that should hold data on student scores.
@@ -13,6 +29,8 @@ def get_scores(dir_name):
         
         if os.path.isfile(path_full):
             with open(path_full, encoding='utf-8') as f:
-                print(json.load(f))
+                student_data = json.load(f)
+                print(sort_scores_by_subject(student_data))
+                
 
 print(get_scores('05_files/json/scores/'))

59e42ab5613242f25ed61fcf2019ec1b9bb998a5
Carrie Clark <carolyn.anne.clark@gmail.com>
2023-07-24 10:05:04 -0500

    reverse_lines: write logic working

--- 05_files/reverse_lines.py
+++ 05_files/reverse_lines.py
@@ -1,11 +1,11 @@
-def reverse_lines(input_file):
+def reverse_lines(input_file, output_file):
     """
     Read the contents of the input file. For each line of the file, reverse
-    the order of the characters and write the reversed line to the output file.
+    the order of characters and write the reversed line to the output file.
     """
     
-    with open(input_file, encoding='utf-8') as input_f:
+    with open(input_file) as input_f, open(output_file, 'w') as output_f:
         for line in input_f:
-            print(''.join(reversed(line.rstrip())))
+            output_f.write(''.join(reversed(line)))
             
-reverse_lines('05_files/wcfile.txt')
+reverse_lines('05_files/wcfile.txt', '05_files/wcfile_reversed.txt')

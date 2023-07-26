f4a64b5b61c5163e839f92f2b885d8aaaee73dd5
Carrie Clark <carolyn.anne.clark@gmail.com>
2023-07-26 16:14:36 -0500

    for loop not needed after all

--- 06_functions/xml_generator.py
+++ 06_functions/xml_generator.py
@@ -1,10 +1,8 @@
 def generate_xml(tag, content, *attributes):
     
-    attr_formatted = ''
-    for attribute in attributes:
-        attribute = attribute.replace(',','')
-        attr_formatted += attribute
+    all_attributes = ''.join(*attributes)
+    attrs_formatted = all_attributes.replace(",", "")
         
-    return f'<{tag} {attr_formatted}>{content}</{tag}>'
+    return f'<{tag} {attrs_formatted}>{content}</{tag}>'
 
-print(generate_xml('emphasis', 'emphasized text', 'italic="yes", underscore="yes"'))
+print(generate_xml('emphasis', 'emphasized text', 'italic="yes"'))

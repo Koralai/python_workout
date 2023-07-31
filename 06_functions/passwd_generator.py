6c9cea71a77404622809fc2c42d71a1ca5c71a0d
Carrie Clark <carolyn.anne.clark@gmail.com>
2023-07-31 16:19:17 -0500

    add docstring, test function calls

--- 06_functions/passwd_generator.py
+++ 06_functions/passwd_generator.py
@@ -1,7 +1,11 @@
 import random
 
 def create_passwd_generator(chars):
-    """..."""
+    """
+    Take a string of allowed characters for a password. Return a function
+    that randomly generates a password of a specified length from characters 
+    in the string.
+    """
     
     def generate_passwd(passwd_length):
         passwd = ''
@@ -11,5 +15,14 @@ def create_passwd_generator(chars):
     
     return generate_passwd
 
-passwd_symb_lett = create_passwd_generator('#!@?_/abcde')
-print(passwd_symb_lett(7))
+simple_passwd_chars = '#!@?abcdefghijklmnopqrstuvwxyz'
+simple_passwd = create_passwd_generator(simple_passwd_chars)
+
+advanced_passwd_chars = '#!@?1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
+advanced_passwd = create_passwd_generator(advanced_passwd_chars)
+
+print(create_passwd_generator(simple_passwd_chars)(12))
+print(create_passwd_generator(advanced_passwd_chars)(12))
+
+print(simple_passwd(15))
+print(advanced_passwd(15))

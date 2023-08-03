f14ecde0df89637934c278cf093fa87e22f0d0ff
Carrie Clark <carolyn.anne.clark@gmail.com>
2023-08-03 14:25:15 -0500

    csv_filter - writing logic works

--- 07_comprehensions/csv_filter.py
+++ 07_comprehensions/csv_filter.py
@@ -1,14 +1,14 @@
 import csv
 
-def get_csv_rows_no_blanks(filename_r, filename_w):
+def write_csv_no_blanks(filename_r, filename_w):
     """
     Read lines of a CSV file. Write all rows that do not contain blanks
     to another CSV file.
     """
-    
+
     with open(filename_r) as read_csv, open(filename_w, 'w') as write_csv:
         file_reader = csv.reader(read_csv, delimiter=',')
-        filtered_rows = []
+        file_writer = csv.writer(write_csv, delimiter=',')
         
         for row in file_reader:
             has_blank_data = False
@@ -17,12 +17,11 @@ def get_csv_rows_no_blanks(filename_r, filename_w):
                 if len(cell) == 0:
                     has_blank_data = True
                     break
-            if has_blank_data is False:
-                filtered_rows.append(row)
                 
-        print(filtered_rows)
+            if has_blank_data is False:
+                file_writer.writerow(row)
             
-get_csv_rows_no_blanks(
+write_csv_no_blanks(
     '05_files/csv_mini_project/lotr_characters.csv',
     '07_comprehensions/lotr_characters_no_blanks.csv')
             

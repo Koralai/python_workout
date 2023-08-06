4a283a5942cbde109f092e68d61295ebb8e305ab
Carrie Clark <carolyn.anne.clark@gmail.com>
2023-08-06 10:02:11 -0500

    transl_pig_latin - works using comprehension

--- 07_comprehensions/translate_pig_latin.py
+++ 07_comprehensions/translate_pig_latin.py
@@ -9,14 +9,11 @@ def pig_latin(word):
 def file_to_pig_latin(file_name):
     """Read a file and return the file's contents translated into pig Latin."""
     
-    output = ""
-    
     with open(file_name, encoding='utf-8') as read_file:
-        for one_line in read_file:
-            words = one_line.split()
-            for word in words:
-                output += f"{pig_latin(word)} "
+        output = [pig_latin(word)
+                  for one_line in read_file
+                  for word in one_line.split()]
                 
-    return output
+    return ' '.join(output)
                         
 print(file_to_pig_latin('02_strings/sun_also_rises.txt'))

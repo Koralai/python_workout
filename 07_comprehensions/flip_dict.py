32fefd9a7a5258cd6204a5e44b82d3326e3a8fa3
Carrie Clark <carolyn.anne.clark@gmail.com>
2023-08-06 10:37:14 -0500

    flip_dict - works with comprehension

--- 07_comprehensions/flip_dict.py
+++ 07_comprehensions/flip_dict.py
@@ -3,13 +3,8 @@ def flip_dict(new_dict):
     Take a dict and flip it (i.e., the original dict's keys become the
     new dict's values).
     """
-    
-    output = {}
-    
-    for key, val in new_dict.items():
-        output[val] = key
-        
-    return output
+    return {val:key
+              for key, val in new_dict.items()}
 
 my_dict = {
     'bilbo': 'hobbit',

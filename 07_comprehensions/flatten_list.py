fc38c014fbd4392c1dfa154d7ea20c8cabb4b1c3
Carrie Clark <carolyn.anne.clark@gmail.com>
2023-08-06 09:35:50 -0500

    flatten_list -- works, but no comprehension

---
+++ 07_comprehensions/flatten_list.py
@@ -0,0 +1,14 @@
+def flatten_list(list_of_lists):
+    """Take a list of lists and return all data as a single list."""
+    
+    output = []
+    
+    for one_list in list_of_lists:
+        for item in one_list:
+            output.append(item)
+            
+    return output
+
+my_list = [[1,2],[3,4]]
+
+print(flatten_list(my_list))

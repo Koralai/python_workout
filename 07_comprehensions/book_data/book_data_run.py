5d9cc61f0bb8c0a71d2b41edad0b6fdf9509142d
Carrie Clark <carolyn.anne.clark@gmail.com>
2023-08-08 10:10:33 -0500

    book data - separate out function call

---
+++ 07_comprehensions/book_data/book_data_run.py
@@ -0,0 +1,9 @@
+from book_data_orig import sci_fi_books
+from book_data_to_dict import book_data_to_dict
+
+def main():
+    print(book_data_to_dict(sci_fi_books))
+
+if __name__ == '__main__':
+    main()
+    
--- 07_comprehensions/book_data/book_data_to_dict.py
+++ 07_comprehensions/book_data/book_data_to_dict.py
@@ -1,5 +1,3 @@
-from book_data_orig import sci_fi_books
-
 def book_data_to_dict(book_data):
     """
     Take a list of tuples containing book data and restructure it as
@@ -10,5 +8,3 @@ def book_data_to_dict(book_data):
             {'lname': book[0], 'fname': book[1], 'price':book[3]}
             }
              for book in book_data]
-
-print(book_data_to_dict(sci_fi_books))

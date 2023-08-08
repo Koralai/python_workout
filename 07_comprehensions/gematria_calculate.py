1aa0369b3d02470cd17a7ed4ebf37cb3808cbb7f
Carrie Clark <carolyn.anne.clark@gmail.com>
2023-08-08 09:24:29 -0500

    get_gematria logic working

---
+++ 07_comprehensions/gematria_calculate.py
@@ -0,0 +1,12 @@
+import gematria_key as gk
+
+def get_gematria(word):
+    """Return the gematria value of a word."""
+    gematria_value = 0
+    
+    for letter in word.lower():
+        gematria_value += gk.gematria_key[letter]
+    
+    return gematria_value
+    
+print(get_gematria('cat'))

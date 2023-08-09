7579bbbde8831ec8fa6f25259b448e0ba5cfc7e0
Carrie Clark <carolyn.anne.clark@gmail.com>
2023-08-09 15:31:23 -0500

    calculate_income_tax appears to be working

--- 08_modules_and_packages/tax_model/income_tax.py
+++ 08_modules_and_packages/tax_model/income_tax.py
@@ -10,7 +10,7 @@ BRACKETS = {
 
 HIGHEST_BRACKET = 7
 
-MIN_PER_BRACKET = {
+MINIMUM_TAX = {
     1: 0,
     2: 1100,
     3: 5147,
@@ -20,7 +20,7 @@ MIN_PER_BRACKET = {
     7: 174238,
 }
 
-MARGINAL_RATE_PER_BRACKET = {
+MARGINAL_RATE = {
     1: 0.1,
     2: 0.012,
     3: 0.22,
@@ -35,25 +35,23 @@ def get_tax_bracket(income):
     
     if income >= BRACKETS[HIGHEST_BRACKET]:
         return HIGHEST_BRACKET   
-    else:
-        for bracket, amt in BRACKETS.items():
-            if income <= amt:
-                return bracket
+
+    for bracket, amt in BRACKETS.items():
+        if income <= amt:
+            return bracket
 
 
 def calculate_income_tax(income):
     """A basic model to estimate income tax based on U.S. tax brackets."""
     
-    RATES = {
-        BRACKETS[1]: 0.1,
-        BRACKETS[2]: MIN_PER_BRACKET[2] + ((income - BRACKETS[1]) * 0.012),
-        BRACKETS[3]: MIN_PER_BRACKET[3] + ((income - BRACKETS[2]) * 0.22),
-        BRACKETS[4]: MIN_PER_BRACKET[4] + ((income - BRACKETS[3]) * 0.24),
-        BRACKETS[5]: MIN_PER_BRACKET[5] + ((income - BRACKETS[4]) * 0.32),
-        BRACKETS[6]: MIN_PER_BRACKET[6] + ((income - BRACKETS[5]) * 0.35),
-        BRACKETS[7]: MIN_PER_BRACKET[7] + ((income - BRACKETS[6]) * 0.37)
-    }
+    bracket = get_tax_bracket(income)
+    prev_bracket = BRACKETS[bracket - 1]
+    min_tax = MINIMUM_TAX[bracket]
+    marg_rate = MARGINAL_RATE[bracket]
     
-    pass
+    if bracket == 1:
+        return income * marg_rate
+    
+    return min_tax + ((income - prev_bracket) * marg_rate)
 
-print(get_tax_bracket(1150000))
+print(calculate_income_tax(400000))

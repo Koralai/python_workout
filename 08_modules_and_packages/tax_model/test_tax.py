4e084ac6564e83fb0ad5c51ca87c4adf0bd1f970
Carrie Clark <carolyn.anne.clark@gmail.com>
2023-08-11 14:44:22 -0500

    add unit tests for income tax functions

---
+++ 08_modules_and_packages/tax_model/test_tax.py
@@ -0,0 +1,16 @@
+from tax import calculate_income_tax
+
+def test_tax_lowest_bracket_income():
+    """Can we calculate the tax for an income in the lowest bracket?"""
+    income = 9000
+    assert calculate_income_tax(income) == 900
+
+def test_tax_mid_bracket_income():
+    """Can we calculate the tax for an income in a middle bracket?"""
+    income = 83000
+    assert calculate_income_tax(income) == 13567.5
+
+def test_tax_highest_bracket_income():
+    """Can we calculate the tax for an income in the highest bracket?"""
+    income = 650000
+    assert calculate_income_tax(income) == 200832

dc492355b6c40d9fd5703eaa84ff215e2783a195
Carrie Clark <carolyn.anne.clark@gmail.com>
2023-08-13 10:14:28 -0500

    create list of scoops more concisely

--- 09_objects/ic_scoop_create.py
+++ 09_objects/ic_scoop_create.py
@@ -3,11 +3,9 @@ from ic_scoop import Scoop
 def create_scoops(flavor_1, flavor_2, flavor_3):
     """Use the Scoop class to create 3 scoops of ice cream."""
     
-    s_1 = Scoop(flavor_1)
-    s_2 = Scoop(flavor_2)
-    s_3 = Scoop(flavor_3)
-    
-    scoops = [s_1, s_2, s_3]
+    scoops = [Scoop(flavor_1), 
+              Scoop(flavor_2), 
+              Scoop(flavor_3)]
     
     print("Scoops created:")
     for scoop in scoops:

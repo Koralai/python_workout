dc80174c330246b17e3df5654f7ada2e9cbecc00
Carrie Clark <carolyn.anne.clark@gmail.com>
2023-08-14 20:06:11 -0500

    add max_scoops attribute to Bowl

--- 09_objects/ic_bowl.py
+++ 09_objects/ic_bowl.py
@@ -3,10 +3,14 @@ from ic_scoop import Scoop
 class Bowl:
     def __init__(self):
         self.current_scoops = []
+        self.max_scoops = 3
     
     def add_scoops(self, *new_scoops: Scoop):
+        num_scoops = len(self.current_scoops)
         for scoop in new_scoops:
-            self.current_scoops.append(scoop)
+            num_scoops += 1
+            if num_scoops <= self.max_scoops:
+                self.current_scoops.append(scoop)
     
     def __repr__(self):
         return ', '.join(s.flavor for s in self.current_scoops)
@@ -14,8 +18,10 @@ class Bowl:
 s_1 = Scoop('coffee')
 s_2 = Scoop('vanilla')
 s_3 = Scoop('chocolate fudge')
+s_4 = Scoop('caramel swirl')
 
 b = Bowl()
 b.add_scoops(s_1)
 b.add_scoops(s_2, s_3)
+b.add_scoops(s_4)
 print(b)

4fd1c6f578794960214834109a9a95bbf1f44833
Carrie Clark <carolyn.anne.clark@gmail.com>
2023-08-14 19:33:49 -0500

    separate Book/Shelf from objects made from them

--- 09_objects/book.py
+++ 09_objects/book.py
@@ -29,14 +29,3 @@ class Shelf:
         titles = [one_book.title
                   for one_book in self.current_books]
         return f"Books on this shelf: {', '.join(titles).title()}" 
-
-b_1 = Book('the merchant of venice', 'william', 'shakespeare', 10)
-b_2 = Book('beloved', 'toni', 'morrison', 14)
-b_3 = Book('little women', 'louisa may', 'alcott', 9)
-
-shelf_1 = Shelf()
-shelf_1.add_book(b_1, b_2)
-shelf_1.add_book(b_3)
-print(shelf_1)
-print(f"Total cost of the books on this shelf: ${shelf_1.total_price():.2f}")
-print(shelf_1.has_book('romeo and juliet'))

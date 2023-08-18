91546ca2271f64d709b364e394307daa15aa39cb
Carrie Clark <carolyn.anne.clark@gmail.com>
2023-08-18 16:36:19 -0500

    refactor Envelope with postage_needed()

--- 09_objects/postage.py
+++ 09_objects/postage.py
@@ -1,18 +1,17 @@
 class Envelope:
-    FOREVER_STAMP = 0.63
-    
-    def __init__(self):
+    def __init__(self, weight=0.4):
         self.was_sent = False
         self.postage = 0
+        self.weight = weight
     
     def postage_needed(self):
-        return self.FOREVER_STAMP
+        return self.weight
     
     def add_postage(self, new_postage):
         self.postage += new_postage
     
     def send(self):
-        if self.postage == self.postage_needed():
+        if self.postage >= self.postage_needed():
             self.was_sent = True
             print('Letter sent.')
         else:

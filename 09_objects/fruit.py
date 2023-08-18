17782418b6dc41cc2b8af53fca8cc8bd38b743cf
Carrie Clark <carolyn.anne.clark@gmail.com>
2023-08-18 16:11:22 -0500

    fruit flavor override working

--- 09_objects/fruit.py
+++ 09_objects/fruit.py
@@ -1,11 +1,20 @@
 class Fruit:
     FLAVOR = 'sweet'
+    
+    def __init__(self, name):
+        self.name = name
+    
+    def describe(self):
+        print(f"This {self.name} is {self.FLAVOR}.")
 
 class Citrus(Fruit):
     FLAVOR = 'tangy'
 
-apple = Fruit()
-orange = Citrus()
+f_1 = Fruit('banana')
+f_2 = Citrus('orange')
 
-print(apple.FLAVOR)
-print(orange.FLAVOR)
+print(f_1.FLAVOR)
+print(f_2.FLAVOR)
+
+f_1.describe()
+f_2.describe()

55d19ba58c8ab70832ca5bfc0b31586396c5f3e6
Carrie Clark <carolyn.anne.clark@gmail.com>
2023-08-19 10:55:30 -0500

    add FOREVER_STAMP

--- 09_objects/postage.py
+++ 09_objects/postage.py
@@ -1,3 +1,5 @@
+FOREVER_STAMP = 0.63
+
 class Envelope:
     def __init__(self, weight=0.4):
         self.was_sent = False
@@ -5,7 +7,7 @@ class Envelope:
         self.weight = weight
     
     def postage_needed(self):
-        return self.weight
+        return FOREVER_STAMP
     
     def add_postage(self, new_postage):
         self.postage += new_postage
@@ -26,7 +28,7 @@ class BigEnvelope(Envelope):
 
 my_letter = Envelope()
 my_letter.send()
-my_letter.add_postage(0.63)
+my_letter.add_postage(FOREVER_STAMP)
 my_letter.send()
 
 big_letter = BigEnvelope()

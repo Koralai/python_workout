5b31d987f824eef1d45eb1c70ddb73f480bb6754
Carrie Clark <carolyn.anne.clark@gmail.com>
2023-08-19 11:34:47 -0500

    ensure conversion to int only occurs when possible

--- 09_objects/flexible_dict.py
+++ 09_objects/flexible_dict.py
@@ -1,10 +1,24 @@
 class FlexibleDict(dict):
+    """
+    A dictionary that uses type conversion to look up keys.
+    Example: dict[1] and dict['1'] are both valid.
+    """
+    
     def __getitem__(self, __key):
+        
         if __key not in self:
-            if int(__key) in self:
-                return super().__getitem__(int(__key))
-            elif str(__key) in self:
+            
+            # check if the key exists if converted to a string
+            if str(__key) in self:
                 return super().__getitem__(str(__key))
+            
+            # only convert the key to an int if it's a string of numbers               
+            elif isinstance(__key, str) and __key.isdigit():
+                
+            # check if the key exists if converted to an int
+                if int(__key) in self:
+                    return super().__getitem__(int(__key))
+            
             else:
                 return 'Key not found.'
         
@@ -17,3 +31,5 @@ print(fd['1'])
 
 fd['23'] = 'hello'
 print(fd[23])
+
+print(fd['#'])

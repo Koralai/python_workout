74e68123806fb6ec875959b2fe6b6e2efd0f6db5
Carrie Clark <carolyn.anne.clark@gmail.com>
2023-08-19 11:44:43 -0500

    cut redundant comments (handled in docstring)

--- 09_objects/flexible_dict.py
+++ 09_objects/flexible_dict.py
@@ -7,14 +7,11 @@ class FlexibleDict(dict):
     def __getitem__(self, key):
         
         if key not in self:
-            
-            # check if the key exists if converted to a string
             if str(key) in self:
                 key = str(key)
             
             # only convert the key to an int if it's a string of numbers               
             elif isinstance(key, str) and key.isdigit():
-            # check if the key exists if converted to an int
                 if int(key) in self:
                     key = int(key)
             

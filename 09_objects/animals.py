81cc52d3af0d1c927204120600e3f0243adb58ca
Carrie Clark <carolyn.anne.clark@gmail.com>
2023-08-21 20:34:35 -0500

    use __class__.__name__ for species

--- 09_objects/animals.py
+++ 09_objects/animals.py
@@ -1,28 +1,31 @@
 class Animal:
-    def __init__(self, color, species='', number_of_legs=0):
+    def __init__(self, color, number_of_legs):
         self.color = color
-        self.species = species
+        self.species = self.__class__.__name__
         self.number_of_legs = number_of_legs
         
     def __repr__(self):
         return f"A {self.color} {self.species} with {self.number_of_legs} legs"
 
 class Sheep(Animal):
-    def __init__(self, color, species='sheep', number_of_legs=4):
-        super().__init__(color, species, number_of_legs)
+    def __init__(self, color):
+        super().__init__(color, number_of_legs=4)
 
 class Wolf(Animal):
-    def __init__(self, color, species='wolf', number_of_legs=4):
-        super().__init__(color, species, number_of_legs)
+    def __init__(self, color):
+        super().__init__(color, number_of_legs=4)
 
 class Snake(Animal):
-    def __init__(self, color, species='snake', number_of_legs=0):
-        super().__init__(color, species, number_of_legs)
+    def __init__(self, color):
+        super().__init__(color, number_of_legs=0)
 
 class Parrot(Animal):
-    def __init__(self, color, species='parrot', number_of_legs=2):
-        super().__init__(color, species, number_of_legs)
+    def __init__(self, color):
+        super().__init__(color, number_of_legs=2)
 
 
 henry = Sheep('black')
 print(henry)
+daisy = Parrot('green')
+print(daisy.number_of_legs)
+print(daisy)

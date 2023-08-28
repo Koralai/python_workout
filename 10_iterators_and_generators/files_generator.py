4a5e81ed822b977ad2378c25a91f9c422aa26d2b
Carrie Clark <carolyn.anne.clark@gmail.com>
2023-08-28 15:12:44 -0500

    files_generator - logic works using for loop

--- 10_iterators_and_generators/files_generator.py
+++ 10_iterators_and_generators/files_generator.py
@@ -4,6 +4,11 @@ def read_files_in_dir(dir_name):
     dir_contents = [one_file 
                     for one_file in os.listdir(dir_name) 
                     if os.path.isfile(dir_name + one_file)]
-    print(dir_contents)
+    
+    for one_file in dir_contents:
+        file_path = dir_name + one_file
+        with open(file_path, encoding="utf-8") as current_file:
+            for one_line in current_file:
+                print(one_line.strip())
     
 read_files_in_dir('08_modules_and_packages/')

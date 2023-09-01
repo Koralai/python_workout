a39a5df9864d214691f7da6df469c0ebdbe5161e
Carrie Clark <carolyn.anne.clark@gmail.com>
2023-09-01 14:48:29 -0500

    elapsed_since works, but for time since start of current iteration

---
+++ 10_iterators_and_generators/elapsed_since.py
@@ -0,0 +1,18 @@
+from time import perf_counter
+
+def iter_time_elapsed(iterable):
+    """
+    A generator function that loops through an iterable and yields the time
+    elapsed since the previous iteration.
+    """
+    
+    for element in iterable:
+        t_start = perf_counter()
+        t_stop = perf_counter()
+        t_elapsed = t_stop - t_start
+        
+        yield (t_elapsed, element)
+
+for a_letter in iter_time_elapsed('to be or not to be'):
+    print(a_letter)
+    
